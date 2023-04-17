1b7741b740b925348ad4b0a747673989af7f19c9
Firmin Launay <hey@firminlaunay.me>
2023-04-17 11:36:41 +0200

    Create main webpage

--- pi/app.py
+++ pi/app.py
@@ -7,7 +7,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        greeting="Hello",
+        name="talented developer",
+        temp_string="Température",
+        temp="19",
+        temp_unit="°C",
+        air_quality_string="Qualité de l’air",
+        air_quality="0",
+        air_quality_unit="/3",
+        pressure_string="Pression ambiante",
+        pressure="1,01",
+        pressure_unit="bar"
+    )
 
 
 @app.route("/add_data/", methods=['POST'])
