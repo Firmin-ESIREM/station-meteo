958553a68059203cfe81ea32dce2898038468919
Firmin Launay <hey@firminlaunay.me>
2023-04-28 10:57:37 +0200

    Create archive webpage

--- pi/app.py
+++ pi/app.py
@@ -1,6 +1,7 @@
 """This is the Flask app."""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
@@ -9,18 +10,43 @@ app = Flask(__name__)
 def home():
     return render_template(
         "index.html",
-        greeting="Hello",
-        name="talented developer",
+        greeting="Bonjour",
+        name="Firmin",
         temp_string="Température",
-        temp="19",
+        temp="19,2",
         temp_unit="°C",
         air_quality_string="Qualité de l’air",
         air_quality="0",
         air_quality_unit="/3",
         pressure_string="Pression ambiante",
         pressure="1,01",
-        pressure_unit="bar"
+        pressure_unit="\xa0bar"
     )
+
+
+KINDS_OF_DATA = {
+    'temperature': {
+        "name": "Température"
+    },
+    'pressure': {
+        "name": "Pression"
+    },
+    'air_quality': {
+        "name": "Qualité de l’air"
+    }
+}
+
+
+@app.route("/archive/")
+def get_archived_data():
+    data = request.args.get("data")
+    if data is None or data not in KINDS_OF_DATA.keys():
+        return BadRequest
+    else:
+        return render_template(
+            "archive.html",
+            data_name=KINDS_OF_DATA[data]["name"]
+        )
 
 
 @app.route("/add_data/", methods=['POST'])
@@ -29,4 +55,4 @@ def add_data():
 
 
 if __name__ == "__main__":
-    app.run(port=12345)
+    app.run(host="0.0.0.0", port=12345)
