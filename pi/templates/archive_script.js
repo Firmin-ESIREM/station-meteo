253c01169f4478957ee84c1073d54dd10a566131
Firmin Launay <hey@firminlaunay.me>
2023-06-14 18:16:00 +0200

    Fix chart colors

--- pi/templates/archive_script.js
+++ pi/templates/archive_script.js
@@ -6,5 +6,7 @@ const chart = JSC.chart('theChart', {
     series: series,
     axisToZoom: 'x',
     xAxis_defaultTick_enabled: false,
-    legend_visible: false
+    legend_visible: false,
+    box_fill: (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ? "#22202D" : "#FFFFFF",
+    yAxis_alternateGridFill: "rgba(0,0,0,0)"
 });
